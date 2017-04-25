16de6f0c49c54f55a501d718e6dc5292c7cacabe
unknown <sukanta.chakraborty@LMKOLPC150.learningmatekol.local>
2017-04-25 17:09:12 +0530

    final commit

--- js/inversepaint.js
+++ js/inversepaint.js
@@ -3,41 +3,40 @@ function triggerInvert() {
 }
 
 function invertTree(root) {
+
     if (root !== null) {
         {
             temp = root.left;
             root.left = root.right;
             root.right = temp;
-
-            if (root === bst.root) {
-                var nod = new Circle(root.x, root.y, 20, "magenta", "mySecondSVG");
-                var txt = new invText(root.x, root.y, root.value);
+          }
+           currentRootNode=root;
+            if (root===bst.root) {
+                this.root = bst.root;
+                var n1 = new Circle(this.root.x, this.root.y, 20, "magenta","mySecondSVG");
+                var txt = new invText(this.root.x, this.root.y, bst.root.value);
             }
-            if (root.left) {
 
-                if (root.right === null) {
-                    var l1 = new Line(root.x, root.y, root.left.x - 100, root.left.y, "magenta", "mySecondSVG");
-                    var nod = new Circle(root.left.x - 100, root.left.y, 20, "magenta", "mySecondSVG");
-                    var txt = new invText(root.left.x - 100, root.left.y, root.left.value);
-                } else {
-                    var l1 = new Line(root.x, root.y, root.right.x, root.right.y, "magenta", "mySecondSVG")
-                    var nod = new Circle(root.left.x, root.left.y, 20, "magenta", "mySecondSVG");
-                    var txt = new invText(root.left.x, root.left.y, root.right.value);
-                }
+            if(root.left){
+              root.left.x = currentRootNode.x -100 ;
+              root.left.y = currentRootNode.y +70;
+                var l1 = new Line(currentRootNode.x, currentRootNode.y, root.left.x, root.left.y,"red","mySecondSVG");
+                var n1 = new Circle(  root.left.x , root.left.y , 20, "magenta","mySecondSVG");
+                var txt = new invText(root.left.x, root.left.y, root.left.value);
             }
-            if (root.right) {
 
-                if (root.left === null) {
-                    var l1 = new Line(root.x, root.y, root.right.x + 100, root.right.y, "magenta", "mySecondSVG");
-                    var nod = new Circle(root.right.x + 100, root.right.y, 20, "magenta", "mySecondSVG");
-                    var txt = new invText(root.right.x + 100, root.right.y, root.right.value);
-                } else {
-                    var l1 = new Line(root.x, root.y, root.left.x, root.left.y, "magenta", "mySecondSVG");
-                    var nod = new Circle(root.right.x, root.right.y, 20, "magenta", "mySecondSVG");
-                    var txt = new invText(root.right.x, root.right.y, root.left.value);
-                }
+            if(root.right){
+              root.right.x = currentRootNode.x +100 ;
+              root.right.y = currentRootNode.y +70;
+              var l1 = new Line(currentRootNode.x, currentRootNode.y, root.right.x, root.right.y,"red","mySecondSVG");
+                var n1 = new Circle(  root.right.x , root.right.y , 20, "magenta","mySecondSVG");
+                var txt = new invText(root.right.x, root.right.y, root.right.value);
             }
-        }
+
+
+
+
+
         if (root.left !== null) {
             if (hasNode(root.left)) {
                 invertTree(root.left);
@@ -49,9 +48,12 @@ function invertTree(root) {
             }
         }
 
-    }
 
-    console.log(bst);
+
+
+
+  console.log(bst);
+}
 }
 
 function hasNode(root) {
