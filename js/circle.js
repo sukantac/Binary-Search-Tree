95ff6d76b138b265bd136823af49ca87aa024bae
unknown <sukanta.chakraborty@LMKOLPC150.learningmatekol.local>
2017-04-26 12:55:23 +0530

    removing ambiguity

--- js/circle.js
+++ js/circle.js
@@ -1,12 +1,11 @@
-var Circle = function(x, y, radius,color) {
+var Circle = function(x, y, radius, color, id) {
 
     var myCircle = document.createElementNS("http://www.w3.org/2000/svg", "circle"); //to create a circle
-    myCircle.setAttributeNS( null,"cx", x);
-    myCircle.setAttributeNS( null,"cy", y);
-    myCircle.setAttributeNS( null,"r", radius);
-    myCircle.setAttributeNS( null,"fill", color);
-    myCircle.setAttributeNS( null,"stroke", "red");
+    myCircle.setAttributeNS(null, "cx", x);
+    myCircle.setAttributeNS(null, "cy", y);
+    myCircle.setAttributeNS(null, "r", radius);
+    myCircle.setAttributeNS(null, "fill", color);
+    myCircle.setAttributeNS(null, "stroke", "red");
+    document.getElementById(id).appendChild(myCircle);
 
-    document.getElementById("mySVG").appendChild(myCircle);
-      // document.getElementById("mySecondSVG").appendChild(myCircle);
 };
--- js/line.js
+++ js/line.js
@@ -1,14 +1,14 @@
         var svgNS = "http://www.w3.org/2000/svg";
 
-        var Line = function(x1position, y1position, x2position, y2position) {
+        var Line = function(x1position, y1position, x2position, y2position,color,id) {
 
             var myLine = document.createElementNS(svgNS, 'line'); //to create a line
             myLine.setAttributeNS(null, "x1", x1position);
             myLine.setAttributeNS(null, "y1", y1position);
             myLine.setAttributeNS(null, "x2", x2position);
             myLine.setAttributeNS(null, "y2", y2position);
-            myLine.setAttributeNS(null, "fill", "rgba(0, 31, 255, 0.38)");
+            myLine.setAttributeNS(null, "fill", color);
             myLine.setAttributeNS(null, "stroke", "red");
 
-            document.getElementById("mySVG").appendChild(myLine);
+            document.getElementById(id).appendChild(myLine);
         };
