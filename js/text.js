c020dfa1d49857e5fec6e905f371d30cd5a37c0e
Sukanta Chakraborty <sukanta.chakraborty@learningmate.com>
2017-08-24 16:01:14 +0530

    adding search facility

--- js/text.js
+++ js/text.js
@@ -1,4 +1,4 @@
-   var Text = function(txtxpos, txtypos, value) {
+   var Text = function(txtxpos, txtypos, value,txtcolor) {
 
        var textelement = document.createElementNS("http://www.w3.org/2000/svg", 'text'); //To create a text
        textelement.setAttribute('dx', txtxpos - 10);
@@ -6,6 +6,8 @@
        textelement.setAttribute('dy', txtypos);
        txtnode = document.createTextNode(value);
        textelement.appendChild(txtnode);
+       textelement.setAttribute('fill', txtcolor);
+
        document.getElementById("mySVG").appendChild(textelement);
    };
 
